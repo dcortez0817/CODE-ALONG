da580a81e79acb3e2bc627edcaddaa1840db43c5
dcortez0817 <darien.cortez@gmail.com>
2019-07-12 18:09:07 -0500

    completed restaurant site

---
+++ Restaurant-Site/Scripts/restaurantScripts.js
@@ -0,0 +1,5 @@
+// placed in a separate document for practice purposes with external js files
+// bootstrap technically does the validation so this message pretends to send the request
+function sendRequest(){
+    alert("The information provided is valid and has been sent.");
+}
